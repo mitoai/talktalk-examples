a3d1b13263ec86f957df4f93b2722ee6df853f28
Christian Budde Christensen <budde377@gmail.com>
2017-08-28 12:29:34 +0200

    feat: Add initial examples

---
+++ cli/counter/index.js
@@ -0,0 +1,24 @@
+// @flow
+
+import { startCliBot, cliReplier } from '../../utils'
+import type { CliMessage, CliReply } from '../../utils'
+import { Dispatcher, Handler } from 'talktalk'
+
+class CounterHandler extends Handler {
+  async handleFirstMessage (message: CliMessage): Promise<*> {
+    await this.sendMessage({message: 'You have sent 1 message'})
+    return {count: 1}
+  }
+
+  async handleSessionMessage (message: CliMessage, context: { count: number }): Promise<*> {
+    const newCount = context.count + 1
+    await this.sendMessage({message: `You have sent ${newCount} messages`})
+    return {count: newCount}
+  }
+}
+
+const dispatcher: Dispatcher<CliMessage, CliReply> = new Dispatcher(cliReplier)
+
+dispatcher.registerHandler(CounterHandler)
+
+startCliBot(dispatcher)
---
+++ cli/echo/index.js
@@ -0,0 +1,17 @@
+// @flow
+
+import { cliReplier, startCliBot } from '../../utils'
+import type { CliMessage, CliReply } from '../../utils'
+import { Dispatcher, Handler } from 'talktalk'
+
+class EchoHandler extends Handler {
+  async handleFirstMessage (message: CliMessage): Promise<*> {
+    await this.sendMessage({message: message.message})
+  }
+}
+
+const dispatcher: Dispatcher<CliMessage, CliReply> = new Dispatcher(cliReplier)
+
+dispatcher.registerHandler(EchoHandler)
+
+startCliBot(dispatcher)
