a80179c0bcae73f8450fc3f3952a27e2b1062bb6
Christian Budde Christensen <budde377@gmail.com>
2017-08-28 20:04:22 +0200

    feat: Add web example

--- README.md
+++ README.md
@@ -2,28 +2,39 @@
 
 [![Greenkeeper badge](https://badges.greenkeeper.io/mitoai/talktalk-examples.svg?token=b5b2408a9c3623e5bab6e55b1832a6c23c2db2099e12123cb944a7a921bf6d2b&ts=1503922566493)](https://greenkeeper.io/)
 
-This folder contains several examples on how to use talktalk.
+This folder contains several examples on how to use TalkTalk.
 
-## [Counter](./cli/counter)
+## cli
+
+### [Counter](./cli/counter)
 
 This example is a simple counter. It illustrates how to structure a very simple 
 conversation utilizing session.
 
 
-## [Echo](./cli/echo)
+### [Echo](./cli/echo)
 
 The echo example just repeats incoming messages. This is the simplest type of conversation, using no 
 sessions.
 
-## [Gif](./cli/gif)
+### [Gif](./cli/gif)
 
 This bot will fetch gifs from a given search term. It illustrates how to integrate the bot
 with a NLU service (Wit.ai) and an external resource provider (Giphy).
 
-## [Jump](./cli/jump)
+### [Jump](./cli/jump)
 
 The Jump example illustrates how one could use jumps for code reuse in advanced conversational flow.
 
-## [Timer](./cli/timer)
+### [Timer](./cli/timer)
 
 This example lets the user set a timer using postbacks.
+
+## web
+
+### [gif](./web/gif)
+
+This bot will fetch gifs from a given search term. It illustrates how to integrate the bot
+with a NLU service (Wit.ai) and an external resource provider (Giphy). 
+
+This will start a webserver serving the bot.
---
+++ web/gif/index.js
@@ -0,0 +1,48 @@
+// @flow
+
+import { Handler } from 'talktalk'
+import { findBestCandidate } from '../utils/wit'
+import { WebWitDispatcher } from '../utils/web'
+import { fetchGif } from '../utils/giphy'
+
+class GreetingHandler extends Handler {
+  intent = 'greeting'
+
+  async handleFirstMessage (msg): Promise<*> {
+    this.sendReply({message: 'Hello there!'})
+    return {}
+  }
+
+}
+
+class GifHandler extends Handler {
+  intent = 'gif'
+
+  async handleFirstMessage (msg): Promise<*> {
+    await this.sendReply({message: 'Sure! What would you like your gif to be about?'})
+    return {}
+  }
+
+  async handleSessionMessage (msg): Promise<*> {
+    const candidate = msg.entities.subject && findBestCandidate(msg.entities.subject)
+    if (!candidate || candidate.confidence < 0.5) {
+      await this.sendReply({message: 'Sorry, I did not understand that. Please try again.'})
+      return {}
+    }
+    await this.sendReply({message: `Ok, I'll see what I can find about "${candidate.value}"`})
+    const gif = await fetchGif(candidate.value)
+    if (!gif) {
+      await this.sendReply({message: `I couldn't find any gif about ${candidate.value}`})
+      return
+    }
+    await this.sendReply({message: 'I found this gif'})
+    await this.sendReply({gif})
+  }
+}
+
+const dispatcher: WebWitDispatcher = new WebWitDispatcher()
+
+dispatcher.registerHandler(GreetingHandler)
+dispatcher.registerHandler(GifHandler)
+
+dispatcher.start()
