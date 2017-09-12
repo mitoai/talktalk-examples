668d1aa4327ecc911465d292cab1f4e271c330a4
Christian Budde Christensen <budde377@gmail.com>
2017-09-12 16:59:54 +0200

    feat: Blank slate

--- web/gif/index.js
+++ web/gif/index.js
@@ -6,13 +6,13 @@ import { Web } from '../../utils/web'
 import { fetchGif, fetchRandomGif } from '../../utils/giphy'
 import type { BaseMessage } from 'talktalk/lib/dispatcher'
 import type { WitEntities } from '../../utils/wit'
+import opn from 'opn'
 
 class GreetingHandler extends Handler {
   intent = 'greeting'
 
   async handleFirstMessage (msg): Promise<*> {
-    this.sendReply({message: 'Hello there!'})
-    return {}
+
   }
 
 }
@@ -21,38 +21,22 @@ class GifHandler extends Handler {
   intent = 'gif'
 
   async handleFirstMessage (msg): Promise<*> {
-    await this.sendReply({message: 'Sure! What would you like your gif to be about?'})
-    return {}
+
   }
 
   async handleSessionMessage (msg): Promise<*> {
-    const candidate = msg.entities.subject && findBestCandidate(msg.entities.subject)
-    if (!candidate || candidate.confidence < 0.5) {
-      await this.sendReply({message: 'Sorry, I did not understand that. Please try again.'})
-      return {}
-    }
-    await this.sendReply({message: `Ok, I'll see what I can find about "${candidate.value}"`})
-    const gif = await fetchGif(candidate.value)
-    if (!gif) {
-      await this.sendReply({message: `I couldn't find any gif about ${candidate.value}`})
-      return
-    }
-    await this.sendReply({message: 'I found this gif'})
-    await this.sendReply({gif})
+
   }
 
   async handlePostback (context: {action: 'gif'}) {
-    const gif = await fetchRandomGif()
-    if (!gif) throw new Error('No gif found')
-    await this.sendReply({message: 'I found this gif'})
-    await this.sendReply({gif})
+
   }
 }
 
 class DefaultHandler extends Handler {
 
   async handleMessage () {
-    this.sendReply({message: 'Sorry, I didn\'t get that.'})
+
   }
 }
 
@@ -63,10 +47,6 @@ export type WebMessage = BaseMessage & { message: string, entities: WitEntities 
 
 const dispatcher: Dispatcher<WebMessage, WebReply> = new Dispatcher((reply, message) => web.sendMessage(message.sender, reply))
 
-dispatcher.registerHandler(GreetingHandler)
-dispatcher.registerHandler(GifHandler)
-dispatcher.registerHandler(DefaultHandler)
-
 web.onMessage(async msg => {
   const entities = await witEntitiesFromMessage(msg.message)
   const intentCandidate = findBestCandidate((entities && entities.intent) || [])
@@ -75,6 +55,6 @@ web.onMessage(async msg => {
   dispatcher.handleMessage(newMessage)
 })
 
-web.onPostback(({sender, context}) => dispatcher.handleMessage(dispatcher.buildPostback(GifHandler, context, sender)))
+web.start()
 
-web.start({name: 'Gif', postbackContext: {action: 'gif'}})
+opn('http://localhost:3001')
